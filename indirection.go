2678542adace86844b8d3ca77a979fc62d2e0269
vpc <cuevasvp@gmail.com>
2020-03-24 21:41:59 +0800

    initial commit

---
+++ indirection.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Vertex structure
+type Vertex struct {
+	X, Y float64
+}
+
+// Scale function
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+// ScaleFunc function
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)
+}
---
+++ method-with-pointer-receivers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Vertex structure
+type Vertex struct {
+	X, Y float64
+}
+
+// Scale function
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+// Abs function
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	fmt.Printf("Before scaling: %+v, Abs: %v\n", v, v.Abs())
+	v.Scale(5)
+	fmt.Printf("After scaling: %+v, Abs: %v\n", v, v.Abs())
+}
